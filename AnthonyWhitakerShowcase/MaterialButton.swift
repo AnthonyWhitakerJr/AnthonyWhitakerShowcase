841537cca4b5bbb4bfd0a889fb21fcaa016575e5
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-11-23 18:56:23 -0500

    Move setting of insets to class file

--- AnthonyWhitakerShowcase/MaterialButton.swift
+++ AnthonyWhitakerShowcase/MaterialButton.swift
@@ -16,6 +16,7 @@ class MaterialButton: UIButton {
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 0, height: 2)
+        contentEdgeInsets = UIEdgeInsetsMake(10, 5, 10, 5)
     }
 
 }
