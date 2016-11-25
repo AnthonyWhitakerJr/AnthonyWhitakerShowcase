432334874b4e8505efe63e727d63dfc5a5cce968
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-11-24 20:06:57 -0500

    Feed UI design.

---
+++ AnthonyWhitakerShowcase/FeedViewController.swift
@@ -0,0 +1,52 @@
+//
+//  FeedViewController.swift
+//  AnthonyWhitakerShowcase
+//
+//  Created by Anthony Whitaker on 11/24/16.
+//  Copyright © 2016 Anthony Whitaker. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+    @IBOutlet weak var feedTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        feedTableView.delegate = self
+        feedTableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension FeedViewController: UITableViewDelegate {
+    
+}
+
+extension FeedViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "postCell") as! PostTableViewCell
+    }
+}
---
+++ AnthonyWhitakerShowcase/PostTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  PostTableViewCell.swift
+//  AnthonyWhitakerShowcase
+//
+//  Created by Anthony Whitaker on 11/24/16.
+//  Copyright © 2016 Anthony Whitaker. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var likeImage: UIImageView!
+    @IBOutlet weak var likeCountLabel: UILabel!
+    @IBOutlet weak var postText: UITextView!
+    @IBOutlet weak var postImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    override func draw(_ rect: CGRect) {
+        profileImage.layer.cornerRadius = profileImage.frame.size.width / 2
+//        postImage.sizeToFit()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
