77202808f2da149a522168c30a7667c6d531269e
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-11-28 22:44:15 -0500

    Fix handling of malformed data from database.

--- AnthonyWhitakerShowcase/Post.swift
+++ AnthonyWhitakerShowcase/Post.swift
@@ -9,10 +9,10 @@
 import Foundation
 
 class Post {
-    private var _postDescription: String!
+    private var _postDescription: String
     private var _imageUrl: String?
-    private var _likes: Int!
-    private var _username: String!
+    private var _likes: Int
+    private var _username: String
     private var _postKey: String!
     
     var postDescription: String {
@@ -35,7 +35,6 @@ class Post {
         return _postKey
     }
     
-    //
     init(username: String, description: String, imageUrl: String? = nil, likes: Int = 0) {//, postKey: String) {
         _username = username
         _postDescription = description
@@ -44,23 +43,21 @@ class Post {
 //        _postKey = postKey // ????
     }
     
-    //FIXME: Does not handle incomplete or corrupt data.
-    init(postKey: String, data: Dictionary<String, Any>) {
-        if let description = data["description"] as? String {
-            self._postDescription = description
+    
+    /// Used to create a Post based on an entry in Firebase database.
+    /// - returns `nil` if data is incomplete, corrupt or malformed.
+    convenience init?(postKey: String, data: Dictionary<String, Any>) {
+        let username = data["username"] as? String
+        let description = data["description"] as? String
+        let imageUrl = data["imageUrl"] as? String
+        let likes = data["likes"] as? Int
+        
+        if let username = username, let description = description, let likes = likes {
+            self.init(username: username, description: description, imageUrl: imageUrl, likes: likes)
+            self._postKey = postKey
         }
         
-        self._imageUrl = data["imageUrl"] as? String
-        
-        if let likes = data["likes"] as? Int {
-            self._likes = likes
-        }
-        
-        if let username = data["username"] as? String {
-            self._username = username
-        }
-        
-        self._postKey = postKey
+        return nil
     }
 
 }
